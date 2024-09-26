f6a375f0d0d4a1159a84bf4c4113711fb2d82f12
John Källström <johnkaellstrom@gmail.com>
2024-09-26 14:13:20 +0200

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home.component';
+import { Posts } from './pages/posts/posts.component';
+import { Categories } from './pages/categories/categories.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home, title: 'Outpost - Home' },
+    { path: 'posts', component: Posts, title: 'Outpost - Posts' },
+    { path: 'categories', component: Categories, title: 'Outpost - Categories' }
+];
---
+++ src/app/shared/navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'navbar',
+    templateUrl: 'navbar.component.html'
+})
+export class Navbar {};
